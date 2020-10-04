f6dc851bd5fe3b86d9147f987382fcb77b8abb3f
Siu <SiuTongYe@gmail.com>
2020-10-04 00:58:47 -0400

    Created two method, remove all the auto-generated components from the constructor to the initComponent() method and a createEvents() methods that will handle events

--- smart-home-simulation/src/windowBuilder/views/SHSGui.java
+++ smart-home-simulation/src/windowBuilder/views/SHSGui.java
@@ -31,12 +31,28 @@ public class SHSGui extends JFrame {
 	 * Create the frame.
 	 */
 	public SHSGui() {
+		initComponents();
+		createEvents();
+	}
+
+	//////////////////////////////////////////////////////////////
+	// This method contains all of the code for creating and
+	// initializing components.
+	//////////////////////////////////////////////////////////////
+	private void initComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+	}
+
+	//////////////////////////////////////////////////////////////
+	// This method contains all of the code for creating events
+	//////////////////////////////////////////////////////////////
+	private void createEvents() {
+
 	}
 
 }

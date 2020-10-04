12b875144cca7a5fcc4d2678ce247a2a07cb83be
Siu <SiuTongYe@gmail.com>
2020-10-04 01:18:57 -0400

    set title to 'smart home simulation'

--- smart-home-simulation/src/windowBuilder/views/SHSGui.java
+++ smart-home-simulation/src/windowBuilder/views/SHSGui.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class SHSGui extends JFrame {
 
@@ -40,6 +41,7 @@ public class SHSGui extends JFrame {
 	// initializing components.
 	//////////////////////////////////////////////////////////////
 	private void initComponents() {
+		setTitle("Smart Home Simulation");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();

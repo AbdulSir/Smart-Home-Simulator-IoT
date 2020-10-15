d7e1e0d8d5a88a74a081a840c61aeb99499d2103
Sujan Saravanamuthu <sujansaravanamuthu@gmail.com>
2020-10-15 19:23:38 -0400

    changed UI of delete

--- smart-home-simulation/src/main/java/view/DeleteUser.java
+++ smart-home-simulation/src/main/java/view/DeleteUser.java
@@ -6,19 +6,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 
 import controller.Users;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 
 public class DeleteUser extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField deleteUserInput;
+	private JComboBox comboBoxDeleteUser;
+	private JLabel deleteUserDropDownLabel;
 
 	/**
 	 * Launch the application.
@@ -51,29 +57,65 @@ public class DeleteUser extends JFrame {
 		deleteUserButton.addMouseListener(new MouseAdapter() {
 			// Delete User function
 			public void mouseClicked(MouseEvent e) {
-				System.out.println("button clicked");
-				String deleteUser = deleteUserInput.getText();
-				System.out.println(deleteUser);
+//				System.out.println("button clicked");
+//				String deleteUser = deleteUserInput.getText();
+//				System.out.println(deleteUser);
+//				Users delete = new Users();
+//				ArrayList<Users> userList = delete.getUserList();
+//				for (Users user: userList) {
+//					if(user.getName().equalsIgnoreCase(deleteUser)) {
+//						userList.remove(user);
+//						deleteUserInput.setText("User Deleted");
+//						break;						
+//					}
+//					else {
+//						deleteUserInput.setText("No User Found");
+//					}
+//				}
+				String userToDelete = comboBoxDeleteUser.getSelectedItem().toString();
 				Users delete = new Users();
 				ArrayList<Users> userList = delete.getUserList();
 				for (Users user: userList) {
-					if(user.getName().equalsIgnoreCase(deleteUser)) {
+					if(user.getName().equalsIgnoreCase(userToDelete)) {
 						userList.remove(user);
-						deleteUserInput.setText("User Deleted");
+					System.out.println("User Deleted");
 						break;						
 					}
 					else {
-						deleteUserInput.setText("No User Found");
+						System.out.println("No User Found");
 					}
 				}
 			}
 		});
 		contentPane.add(deleteUserButton, BorderLayout.SOUTH);
+		final Users Admin = new Users();
+		comboBoxDeleteUser = new JComboBox();
+		// Will update User pop up menu every time the user opens the menu 
+	    PopupMenuListener listener = new PopupMenuListener() {
+	        boolean initialized = false;
+
+	        public void popupMenuCanceled(PopupMenuEvent e) {
+	        }
+
+	        public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+	        }
+
+	        public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+	          if (!initialized) {
+					String[] userNameArray = new String[Admin.getUserList().size()];
+					for(int i = 0; i < userNameArray.length; i++) {
+						userNameArray[i] = Admin.getUserList().get(i).getName();
+					}
+					comboBoxDeleteUser.setModel(new DefaultComboBoxModel(userNameArray));
+	          }
+	        }
+	    };
+		comboBoxDeleteUser.addPopupMenuListener(listener);
+	
+		contentPane.add(comboBoxDeleteUser, BorderLayout.CENTER);
 		
-		deleteUserInput = new JTextField();
-		deleteUserInput.setText("Enter UserName You Wish To Delete");
-		contentPane.add(deleteUserInput, BorderLayout.CENTER);
-		deleteUserInput.setColumns(10);
+		deleteUserDropDownLabel = new JLabel("Select User to Delete");
+		contentPane.add(deleteUserDropDownLabel, BorderLayout.NORTH);
 	}
 
 }

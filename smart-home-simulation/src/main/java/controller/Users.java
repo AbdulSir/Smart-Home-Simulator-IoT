14ba93d8b07df3ae285a8f6b237219ab3a5a334a
Siu <SiuTongYe@gmail.com>
2020-10-17 19:14:08 -0400

    Formatted users.java class

--- smart-home-simulation/src/main/java/controller/Users.java
+++ smart-home-simulation/src/main/java/controller/Users.java
@@ -10,6 +10,7 @@ public class Users {
 	private int userNumber;
 	private static int counter = 0;
 	private String location;
+
 	/**
 	 * Class Constructor
 	 */
@@ -19,6 +20,7 @@ public class Users {
 		this.userNumber = 0;
 		this.location = "Hallway";
 	}
+
 	/**
 	 * Class Constructor specifying number of objects to create
 	 */
@@ -52,60 +54,70 @@ public class Users {
 			System.out.println("-----------");
 		}
 	}
+
 	/**
 	 * Getter userList
 	 */
 	public ArrayList<Users> getUserList() {
 		return userList;
 	}
+
 	/**
 	 * Setter userList
 	 */
 	public static void setUserList(ArrayList<Users> userList) {
 		Users.userList = userList;
 	}
+
 	/**
 	 * Getter name
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
 	 * Setter name
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	/**
 	 * Getter location
 	 */
 	public String getLocation() {
 		return location;
 	}
+
 	/**
 	 * Setter location
 	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	/**
 	 * Getter activeUser
 	 */
 	public Boolean getActiveUser() {
 		return activeUser;
 	}
+
 	/**
 	 * Setter activeUser
 	 */
 	public void setActiveUser(Boolean activeUser) {
 		this.activeUser = activeUser;
 	}
+
 	/**
 	 * Getter userNumber
 	 */
 	public int getUserNumber() {
 		return userNumber;
 	}
+
 	/**
 	 * Setter userNumber
 	 */
